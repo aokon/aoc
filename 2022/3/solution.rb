95a09b31d6042e2a6cfb73eb8f88c295f23dd21d
Adam Okoń <adam.okon@elegantcode.pl>
2022-12-04 21:14:27 +0100

    solve part two for #3/22 - yolo mode

--- 2022/3/solution.rb
+++ 2022/3/solution.rb
@@ -10,7 +10,24 @@ class RucksackPlanner
     result = 0
     File.readlines(@file_name).each do |line|
       result += divide_item(line)
-                .then { |parts| calculate_priorities(parts) }
+                .then { |parts| similiar_elements(parts) }
+                .then { |sm| calculate_priorities(sm) }
+    end
+
+    result
+  end
+
+  def part_two
+    result = 0
+    group = []
+
+    File.readlines(@file_name).each.with_index do |line, index|
+      group.push(line.strip)
+
+      if ((index + 1) % 3).zero?
+        result += similiar_group_elements(group).then { |sm| calculate_priorities(sm) }
+        group = []
+      end
     end
 
     result
@@ -36,12 +53,23 @@ class RucksackPlanner
     [first, second]
   end
 
-  def calculate_priorities(parts)
-    similiar_elements = parts[1].scan(/[#{parts[0]}]/).uniq
+  def similiar_elements(parts)
+    parts[1].scan(/[#{parts[0]}]/).uniq
+  end
 
+  def calculate_priorities(similiar_elements)
     similiar_elements.reduce(0) do |memo, item|
       memo += @priorities_score[item]
       memo
     end
+  end
+
+  def similiar_group_elements(group)
+    second_group = group[1].chars
+    third_group = group[2].chars
+
+    group[0].chars
+            .select { |c| second_group.include?(c) && third_group.include?(c) }
+            .uniq
   end
 end
