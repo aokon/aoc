4f033e994da2bbd912d5ad0b7c19cf53c5019d14
Adam Okoń <adam.okon@elegantcode.pl>
2023-12-01 23:02:32 +0100

    start new game in 2023 :D

---
+++ 2023/1/day_one_test.rb
@@ -0,0 +1,38 @@
+require 'minitest/autorun'
+require_relative 'day_one'
+
+describe DayOne do
+  describe "self.part_one" do
+    before do
+      @input = <<-INPUT
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+      INPUT
+    end
+
+    it "returns valid result result" do
+      _(DayOne.part_one(@input)).must_equal 142
+    end
+  end
+
+  describe "self.part_two" do
+    before do
+      @input = <<-INPUT
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightsevenseven2
+        zoneight234
+        7pqrstsixteen
+        xksghrh7oneone245cvcnrfgtwoneq
+      INPUT
+    end
+
+    it "returns valid result result" do
+      _(DayOne.part_two(@input)).must_equal 352
+    end
+  end
+end
